c0d6636364050a4d81d2ce2fd9beca73c958c7d3
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-01-25 15:32:56 +0100

    push

---
+++ Gioco dei dadi/js/script.js
@@ -0,0 +1,22 @@
+// Gioco dei dadi 
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+alert('Tiro il dado per il Giocatore 1');
+
+let dadoGiocatore1 = Math.floor(Math.random() * 6 +1);
+console.log(dadoGiocatore1)
+alert('È uscito ' + dadoGiocatore1);
+
+alert('Tiro il dado per il Giocatore 2');
+
+let dadoGiocatore2 = Math.floor(Math.random() * 6 +1);
+console.log(dadoGiocatore2)
+alert('È uscito ' + dadoGiocatore2);
+
+if(dadoGiocatore1 > dadoGiocatore2){
+    alert('Ha vinto il giocatore 1');
+}else{
+    alert('Ha vinto il giocatore 2');
+}
---
+++ Mail/js/script.js
@@ -0,0 +1,17 @@
+// Mail 
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// Doamanda email
+let mail = (prompt('Inserire la mail per il controllo.\nGrazie'));
+console.log(mail);
+
+const myArray = ['matteo@gmail.com', 'luca@gmail.com', 'marco@gmail.com', 'giovanni@gmail.com'];
+
+if (mail = myArray) {
+    alert('La sua mail è nella lista e può accedere');
+} else {
+    alert('La sua mail non è nella lista, non può accedere');
+}
