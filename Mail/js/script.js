78655ba2038830e7dee47193d5dd62786db91d20
Matteo Gioele Cocomazzi <TheZefyro@gmail.com>
2022-01-25 15:59:23 +0100

    push test

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -9,9 +9,16 @@ let mail = (prompt('Inserire la mail per il controllo.\nGrazie'));
 console.log(mail);
 
 const myArray = ['matteo@gmail.com', 'luca@gmail.com', 'marco@gmail.com', 'giovanni@gmail.com'];
+console.log(myArray)
 
 if (mail = myArray) {
     alert('La sua mail è nella lista e può accedere');
 } else {
     alert('La sua mail non è nella lista, non può accedere');
 }
+
+
+
+
+
+
